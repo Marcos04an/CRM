7f34c1f15753399f90011657dc73701a7573cd97
Marcos <marcos.cisneiros04@gmail.com>
2026-01-19 12:13:00 -0300

    Exclusao do arquivo menu.tsx

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,22 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import path from 'path'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    parser: parser, 
+    parserOptions: {
+      project: path.resolve('./CRM/tsconfig.json'),
+      tsconfigRootDir: __dirname,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -16,7 +25,6 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
