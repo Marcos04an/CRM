a8a503770a79dd295c85cfd0c053a43f23399fda
Gissany Vieira <gissanyvn@gmail.com>
2026-01-19 01:09:29 -0300

    adicionando código

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -13,10 +13,10 @@ export default function AppRoutes(){
         <Routes>
             
             <Route path="/" element={<Dashboard/>}/>
-             <Route path="/" element={<Enterprise/>}/>
-              <Route path="/" element={<LeadProfile/>}/>
-               <Route path="/" element={<LeadRegistration/>}/>
-                <Route path="/" element={<PropertyRegistration/>}/>
+             <Route path="/empreendimentos" element={<Enterprise/>}/>
+              <Route path="/perfil-lead" element={<LeadProfile/>}/>
+               <Route path="/cadastro-lead" element={<LeadRegistration/>}/>
+                <Route path="/cadastro-imovel" element={<PropertyRegistration/>}/>
         </Routes>
     )
 }
